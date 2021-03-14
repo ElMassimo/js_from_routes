4ef461999eae0ff325d0d435219d42f0d8d4d38b
Maximo Mussini <maximomussini@gmail.com>
2021-03-14 11:21:19 -0300

    test: Remove unnecessary tests for Inertia

--- packages/inertia/test/index.test.ts
+++ packages/inertia/test/index.test.ts
@@ -7,32 +7,6 @@ describe('formatUrl', () => {
   })
 })
 
-describe('request', () => {
-  it.only('can unwrap a JSON response', async () => {
-    expect(await request('get', 'https://pokeapi.co/api/v2/pokemon/:pokemon', { pokemon: 'pikachu' })).toMatchObject({
-      name: 'pikachu',
-    })
-  })
-
-  it('can return the raw response', async () => {
-    const fakeFetch = async (...args: any[]) => ({ status: 200, body: args })
-    expect(await request('get', '/videos/:id/download', { id: 2, fetch: fakeFetch, responseAs: 'response' })).toEqual({
-      status: 200,
-      body: [{
-        method: 'GET',
-        responseAs: 'response',
-        url: '/videos/2/download',
-        data: undefined,
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': undefined,
-        },
-      }],
-    })
-  })
-})
-
 describe('definePathHelper', () => {
   it('returns a path helper with all the properties', async () => {
     const helper = definePathHelper('get', '/videos/:id/download')
@@ -40,18 +14,5 @@ describe('definePathHelper', () => {
     expect(helper.httpMethod).toEqual('get')
     expect(helper.pathTemplate).toEqual('/videos/:id/download')
     expect(helper.path({ i: 2, id: 5 })).toEqual('/videos/5/download')
-
-    const fakeFetch = async (...args: any[]) => ({ status: 200, json: () => Promise.all(args) })
-    expect(await helper({ id: 2, fetch: fakeFetch })).toEqual([{
-      data: undefined,
-      headers: {
-        Accept: 'application/json',
-        ContentType: 'application/json',
-        XCSRFToken: undefined,
-      },
-      method: 'GET',
-      responseAs: 'json',
-      url: '/videos/2/download',
-    }])
   })
 })
