a2a88b48cb75c1f457498295070aa5040b10c634
Maximo Mussini <maximomussini@gmail.com>
2022-03-28 17:11:36 -0300

    chore: remove unused variable

--- packages/inertia/test/index.test.ts
+++ packages/inertia/test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { definePathHelper, request, formatUrl } from '../src'
+import { definePathHelper, formatUrl } from '../src'
 
 describe('formatUrl', () => {
   it('is exported correctly', () => {
